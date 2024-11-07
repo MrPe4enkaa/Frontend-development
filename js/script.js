e6b73733a9528361209b37c6de453e6a788100ef
mrpe4enushkaa_ <daniillavrika1980@gmail.com>
2024-11-07 21:22:00 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //isimage == false && isImage == true
+    function Animation(e) {
+        const element = e;
+        gsap.fromTo(element, {
+            y: 50,
+            opacity: 0,
+            duration: 0.8
+        }, {
+            y: 0,
+            opacity: 1,
+            duration: 0.8
+        });
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                Animation(entry.target);
+                observer.unobserve(entry.target);
+            };
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    document.querySelectorAll('.h').forEach(element => {
+        observer.observe(element);
+    });
+    document.querySelectorAll('.smallH').forEach(element => {
+        observer.observe(element);
+    });
+    document.querySelectorAll('.p').forEach(element => {
+        observer.observe(element);
+    });
+    document.querySelectorAll('#background').forEach(element => {
+        observer.observe(element);
+    });
+});
